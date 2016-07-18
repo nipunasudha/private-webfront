0a6db89555bdf74f07c7e39a187b9b0354956671
nipunasudha <nipuna.14@cse.mrt.ac.lk>
2016-07-18 22:15:02 +0530

    advances search page done! all good, no titles yet

--- js/app.advanced-search.js
+++ js/app.advanced-search.js
@@ -1,60 +1,14 @@
-// VARIABLE ========================
-var sidebarVisible = false
-var isMobile = false
-// EVENTS============================
+function switchWatch () {
 
-$(function () {
-  init_positions()
-  realtime_adjustments()
-})
-$(window).on('resize', function () {
-  realtime_adjustments()
-})
-$(window).on('scroll', function () {})
+  if ($('.watch-active').attr('hidden')) {
+    $('.watch-active').removeAttr('hidden')
+    $('.watch-inactive').attr('hidden', '')
 
-// FUNCTIONS ======================
-function realtime_adjustments () {
-  check_mobile()
-  set_filter_position(isMobile)
-// put code to hide toggler!
-}
-function init_positions () {
-  $('.left-div').width('0%')
-  $('.right-div').width('100%')
-}
-function check_mobile () {
-  isMobile = (window.innerWidth <= 992)
-  console.log(isMobile)
-}
-function showslide () {
-  $('.right-div').stop().animate({width: '75%'}, 300)
-  $('.left-div').stop().animate({width: '25%'}, 300)
-  sidebarVisible = true
-}
-function hideslide () {
-  $('.right-div').stop().animate({width: '100%'}, 300)
-  $('.left-div').stop().animate({width: '0%'}, 300)
-  sidebarVisible = false
-}
-
-function toggleslide () {
-  if (sidebarVisible) {
-    $('.right-div').stop().animate({width: '100%'}, 300)
-    $('.left-div').stop().animate({width: '0%'}, 300)
-    sidebarVisible = false
-  }else {
-    $('.right-div').stop().animate({width: '75%'}, 300)
-    $('.left-div').stop().animate({width: '25%'}, 300)
-    sidebarVisible = true
+  } else {
+       $('.watch-inactive').removeAttr('hidden')
+    $('.watch-active').attr('hidden', '')
+    
   }
-}
 
-function set_filter_position (k) {
-  if (k) {
-    $('.right-div').stop().width('100%')
-    $('.left-div').stop().width('100%')
-  }else {
-    $('.right-div').stop().width('100%')
-    $('.left-div').stop().width('0%')
-  }
+  
 }
