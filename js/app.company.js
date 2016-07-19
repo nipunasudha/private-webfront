f6b690473444f035e521beddfd7a69ef04d50094
nipunasudha <nipuna.14@cse.mrt.ac.lk>
2016-07-20 04:40:44 +0530

    performance fix, company page done, navbar scroll bug fixed, TODO employee search

--- js/app.company.js
+++ js/app.company.js
@@ -1,6 +1,7 @@
 
+
 $('.btn-pin').on('click', function () {
   $(this).toggleClass('btn-pin')
   $(this).toggleClass('btn-pinned')
-  $(this).find(".pin-text").toggle();
+  $(this).find('.pin-text').toggle()
 })
